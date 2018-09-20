b668449bae4f7275c77320b6bb087d3778f494cf
borsics <borscsa1@gmail.com>
2018-09-20 23:53:40 +0200

    majd sleepy dod back did

--- engine.h
+++ engine.h
@@ -9,10 +9,12 @@ class Engine
 {
 public:
 	bool init();
+	bool isFileBlacklisted(const QString &filePath);
 	bool lookup(QString hash);
 	qint16 fileScan(QString path);
 	QMap<QString, QString> hashes(QString path);
 	QString fileHashGenerate(QString path, QCryptographicHash::Algorithm algorithm);
+
 
 
 private:
